bda9763405f5bb72152cfc4823e3088ad195b173
ilias boujlil <iliasboujlil@iliass-MBP.lan>
2023-12-04 22:27:33 -0500

    commit #15

--- app.py
+++ app.py
@@ -9,14 +9,7 @@ from pdf2image import convert_from_bytes
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"": {"origins": "*"}})
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    return response
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 def image_to_base64(image_path):
