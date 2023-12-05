4dfd5127cb29567bfbdc22f102fd7b2500b5852c
ilias boujlil <iliasboujlil@iliass-MBP.lan>
2023-12-04 22:03:34 -0500

    commit #11

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from pdf2image import convert_from_bytes
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})
+app.use(CORS());
 
 
 def image_to_base64(image_path):
